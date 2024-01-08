21d43d9343a6805b98987e85ed1b113605032ebf
Aurora Santos <aurora.sants05@gmail.com>
2024-01-08 20:17:23 -0300

    Implementamos a ideia de polimorfismo, criando um sistema onde as subclasses herdam da classe Smartphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("100", "Win30", "JJAHD5714", 64);
+nokia.InstalarAplicativo("DIO");
+nokia.ReceberLigacao();
+nokia.Ligar();
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("120", "ProMax", "HAJDH6846", 128);
+iphone.InstalarAplicativo("DIO");
+iphone.ReceberLigacao();
+iphone.Ligar();
